1b6cec08b20503c716313c280ee656b36b1c802e
Raashee <110528524+Raashee@users.noreply.github.com>
2022-08-09 18:02:33 +0530

    Update script.js

--- yt/script.js
+++ yt/script.js
@@ -1,5 +1,3 @@
-
-
 let getJSON = (url, callback) => {
 
     let xhr = new XMLHttpRequest();
@@ -19,32 +17,29 @@ let getJSON = (url, callback) => {
 
     xhr.send();
 };
-
 $(document).on('click','.send_form', function(){
-  
-      
+
+  var Url = "https://api-xcoders.xyz/api/download/ytmp4?url=",
+      Aurl = "https://api-xcoders.xyz/api/download/ytmp3?url=",
+      key = "&apikey=v7Al5I5obp"
   yt = $("#ytl").val(),
-  
   form = $('#format').val();
   
   if (form != "mp4") {
-    
-    let id = yt.replace("https://youtu.be/", "");
-
-    var text = "https://ytmp3api.cyou/mload?ytid=" + id + "&bit=128&url=24327924313024394535694d416d376d514b2f42567a61556b746543754d3977362e4c6a4c6e634b34634e656b7a356e542e4b4b3875486f422f4665"
-    window.open(text, '_blank');
+      getJSON(Aurl + yt + key, (err, data) => {
+        let text = `${data.result.url}`
+        window.open(text, '_blank');
         console.log(text);
         
-  }
-    else {
-      
-     let id = yt.replace("https://youtu.be/", "");
-
-    var text = "https://ytmp3api.cyou/dload?ytid=" + id + "&res=720&url=243279243130244f62424549777564302e57446467637a68484c584e656633692e334a4f5431325334432f57732e412f3149592e6a4e514f614b3669"
-    window.open(text, '_blank');
-        console.log(text);
-     
-    }
-
 });
-
+    }
+    else {
+      getJSON(Url + yt + key, (err, data) => {
+let text = `${data.result.url}`
+window.open(text, '_blank');
+        console.log(text);
+        
+});
+}
+    
+});
